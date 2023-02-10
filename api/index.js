d472092c2157ba70cec5d7a7f0123174ebfbdd7a
Sean Marney <marneys@cardiff.ac.uk>
2023-02-10 15:10:11 +0000

    Created tests for getRewards method in controller and the /management/rewards GET request

--- api/index.js
+++ api/index.js
@@ -24,3 +24,5 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
--- api/routes/manageRewards.js
+++ api/routes/manageRewards.js
@@ -2,7 +2,6 @@ const express = require("express");
 const {
   createReward,
   deleteReward,
-  // getRewardByName,
   getRewards,
   getRewardSortedByStars,
   updateReward,
