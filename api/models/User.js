64a1c59a364f24d0830000d2abe2a8f030fafc62
c1986023 <Sonyw910i->
2023-02-11 23:08:33 +0000

    Create a schema to represent the user

--- api/models/User.js
+++ api/models/User.js
@@ -1,1 +1,23 @@
-const mongoose = require("mongoose");
+import { Schema, model } from "mongoose";
+
+let userSchema = Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = model("User", userSchema);
+export default User;
