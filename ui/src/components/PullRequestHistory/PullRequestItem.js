01fd8120316d76220e1a57fcec23c0a8d443aa5f
Owain Lansdowne <lansdowneop@cardiff.ac.uk>
2023-02-13 17:47:01 +0000

    About to merge in develop

--- ui/src/components/PullRequestHistory/PullRequestItem.js
+++ ui/src/components/PullRequestHistory/PullRequestItem.js
@@ -4,8 +4,10 @@ export class PullRequestItem extends React.Component {
   render() {
     return (
         <div>
+            <p>13/02/2023 17:34</p>
             <h1>Solved Navbar Bug</h1>
             <a href={this.props.pullRequest.url}>#{this.props.pullRequest._id}</a>
+            <h3>{this.props.pullRequest.ratings.overall} Stars</h3>
         </div>
     );
   }
