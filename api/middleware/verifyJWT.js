e29d31c91a5c2ffabe4a8d525442e0c4b000b3fc
c1986023 <Sonyw910i->
2023-02-14 22:47:10 +0000

    Add user routes

--- api/middleware/verifyJWT.js
+++ api/middleware/verifyJWT.js
@@ -2,9 +2,9 @@ const jwt = require("jsonwebtoken");
 
 // function checks if a JSON Web Token (JWT) exists in the "x-access-token" header of an HTTP request
 function verifyJWTToken(req, res, next) {
-    
+
     // Get the JWT from the "x-access-token" header of the request
-    const token = req.headers["x-access-token"]?.split(" ")[1];
+    const token = req.headers["x-access-token"];
 
     // If no token is provided, return an error response
     if (!token) {
