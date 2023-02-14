ea22b03c5ac3b12153a2c52212ffae78ab984173
c1986023 <Sonyw910i->
2023-02-14 18:53:28 +0000

    Add comments to the loginUser function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -24,29 +24,37 @@ const registerUser = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
+    // Extract email and password values from the request body
     const { email, password } = req.body;
 
     try {
+        // Look up the user in the database using the provided email (convert to lowercase to ensure consistency)
         const user = await User.findOne({ email: email.toLowerCase() });
         if (!user)
+            // If user is not found, return a 401 response with an error message
             return res
                 .status(401)
                 .json({ message: "Invalid email or password" });
 
+        // Compare the password with the hashed password stored in the database
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch)
+            // If the passwords don't match, return a 401 response with an error message
             return res
                 .status(401)
                 .json({ message: "Invalid email or password" });
 
+        // Generate a JSON Web Token (JWT) with the user's ID and email and sign it with a secret key
         const token = jwt.sign(
             { id: user._id, email: user.email },
             process.env.PASSPORTSECRET,
             { expiresIn: 86400 }
         );
 
+        // Return a success response with the JWT included as a Bearer token
         res.json({ message: "Success", token: `Bearer ${token}` });
     } catch (error) {
+        // If there is an error, log it to the console and return a 500 response with an error message
         console.error(error);
         res.status(500).json({ message: "Server Error" });
     }
