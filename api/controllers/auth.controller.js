8ae914fe6cf72d14603f362557dd33202caac1f2
c1986023 <Sonyw910i->
2023-02-14 18:49:56 +0000

    Added comments to explain the functionality of the function registerUser

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,24 @@
 const User = require("../models/userModel");
 
 const registerUser = async (req, res) => {
+    // Extract the name and email values from the request body and convert to lowercase
     const name = req.body.name.toLowerCase();
     const email = req.body.email.toLowerCase();
+
+    // Check if a user with the same name or email already exists in the database
     const existingUser = await User.findOne({ $or: [{ name }, { email }] });
     if (existingUser) {
+        // If a user with the same name or email exists, return a 401 response with an error message
         return res
             .status(401)
             .json({ message: "Name or email has already been taken" });
     }
+
+    // Hash the password with bcrypt and create a new User document with the name, email, and hashed password
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const newUser = new User({ name, email, password: hashedPassword });
+
+    // Save the new user to the database and return a success response
     await newUser.save();
     res.json({ message: "Success" });
 };
