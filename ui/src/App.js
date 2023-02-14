a73b363831e87f16728cc25345b53d741002eabb
c1986023 <Sonyw910i->
2023-02-14 18:25:42 +0000

    Added routes for register and login pages

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./components/pages/signIn/Register";
+import Login from "./components/pages/signIn/Login";
+import Dashboard from "./components/pages/Dashboard";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
+        <div className="bg-gray-900 min-h-screen">
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Register />} exact path="/register" />
+                    <Route element={<Login />} exact path="/login" />
+                    <Route element={<Dashboard />} exact path="/dashboard" />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
