985c3a578d84b6b7c4af3326139c3625525d6352
c1986023 <Sonyw910i->
2023-02-14 22:56:25 +0000

    Added dashboard page with useffect hook- extracts the token from the cookies object, and sends a GET request to the users endpoint with the token as a header

--- ui/src/components/pages/Dashboard.js
+++ ui/src/components/pages/Dashboard.js
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+
 function Dashboard() {
+    const [cookies, setCookie] = useCookies([]);
+
+    useEffect(async () => {
+        const response = await axios.get(
+            `${process.env.REACT_APP_API_ENDPOINT}/users`,
+            {
+                headers: {
+                    "x-access-token": cookies.token.split(" ")[1],
+                },
+            }
+        );
+        console.log(response);
+    }, []);
+
     return (
         <div>
-            <h1>Redirected</h1>
+            <h1>Redirected after login</h1>
         </div>
     );
 }
