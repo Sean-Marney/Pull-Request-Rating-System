e2cb023275f0a7f252b776b40f3aaded1707d729
Sean Marney <marneys@cardiff.ac.uk>
2023-02-14 14:07:23 +0000

    Revert "Created test and updated jest configuration"
    This reverts commit f0a4c63cb2366d876fa69ca557b191434ff86503.

--- ui/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // I have no idea if any of this is correct
-  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
-};
