4c1f293843f18e26a4201d8e6ba5aa036282cd56
c1986023 <Sonyw910i->
2023-02-14 18:22:27 +0000

    Add if condition to check whether the token is provided

--- api/middleware/verifyJWT.js
+++ api/middleware/verifyJWT.js
@@ -5,6 +5,16 @@ function verifyJWTToken(req, res, next) {
     
     // Get the JWT from the "x-access-token" header of the request
     const token = req.headers["x-access-token"]?.split(" ")[1];
+
+    // If no token is provided, return an error response
+    if (!token) {
+        return res.json({
+            message: "Incorrect Token Given",
+            isLoggedIn: false,
+        });
+    }
+
+    
 }
 
 module.exports = verifyJWTToken;
