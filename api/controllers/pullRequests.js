6a1ac3ba0d3ff2d67b570fbbf5428e5b08058612
Owain Lansdowne <lansdowneop@cardiff.ac.uk>
2023-02-14 17:17:24 +0000

    sorts the list by date

--- api/controllers/pullRequests.js
+++ api/controllers/pullRequests.js
@@ -5,7 +5,8 @@ const PullRequest = require("../models/PullRequest");
 const getPullRequestsForUser = async (req, res) => {
   try {
     const  parameter  = req.params;
-    const pullRequests = await PullRequest.find(  { user_id: parameter.id} );
+    var sortByDate = { date: -1 };
+    const pullRequests = await PullRequest.find(  { user_id: parameter.id} ).sort(sortByDate);
     res.status(200).json(pullRequests);
   } catch (error) {
     res.status(404).json({ message: error.message });
