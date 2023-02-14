80baca825a781ba1f32f1e570f3369dedcdd9752
c1986023 <Sonyw910i->
2023-02-14 12:14:01 +0000

    Create a register route that handles a POST request to register a new user. Checks whether the email or name exists in the user collection

---
+++ api/routes/authRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const verifyJWT = require("../middleware/verifyJWT");
+
+router.post("/register", async (req, res) => {
+    console.log(req);
+    const user = req.body;
+
+    const takenName = await User.findOne({
+        name: user.name.toLowerCase(),
+    });
+    const takenEmail = await User.findOne({ email: user.email.toLowerCase() });
+
+    if (takenName || takenEmail) {
+        return res.status(401).send({
+            message: "Username or email has already been taken",
+        });
+    }
+});
+
+module.exports = router;
