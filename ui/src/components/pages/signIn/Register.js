67cd887320c337598ec13572197a2cc2b1ef8559
c1986023 <Sonyw910i->
2023-02-16 17:03:43 +0000

    Styled the registeration page with material UI

--- ui/src/components/pages/signIn/Register.js
+++ ui/src/components/pages/signIn/Register.js
@@ -1,34 +1,57 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import useAxiosInstance from "../../../useAxiosInstance";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
-function Register() {
+const theme = createTheme();
+
+export default function SignUp() {
     const navigate = useNavigate();
+    const { request } = useAxiosInstance();
+    const [user, setUser] = React.useState({
+        email: "",
+        name: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUser((prevUser) => ({
+            ...prevUser,
+            [name]: value,
+        }));
+    };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const form = event.target;
-        const user = {
-            email: form.email.value,
-            name: form.name.value,
-            password: form.password.value,
-            confirmPassword: form.confirmPassword.value,
-        };
-
-        // Check that the passwords match
+        //Check that the passwords match
         if (user.password !== user.confirmPassword) {
             alert("Passwords do not match");
             return;
         }
 
         try {
-            const response = await axios.post(
-                `${process.env.REACT_APP_API_ENDPOINT}/register`,
-                user
-            );
+            const response = await request({
+                method: "post",
+                url: "/register",
+                data: { ...user },
+            });
+
             if (response.data.isRegistered) {
                 navigate("/login");
             } else {
-                alert(response.data.message);
+                alert("Email or name already exists");
             }
         } catch (error) {
             console.log(error);
@@ -36,24 +59,94 @@ function Register() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input required type="email" name="email" id="email" />
-            <label htmlFor="name">Name</label>
-            <input required type="text" name="name" id="name" />
-            <label htmlFor="password">Password</label>
-            <input required type="password" name="password" id="password" />
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input
-                required
-                type="password"
-                name="confirmPassword"
-                id="confirmPassword"
-            />
-
-            <input type="submit" value="Register" />
-        </form>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: theme.spacing(8),
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box
+                        component="form"
+                        noValidate
+                        onSubmit={handleSubmit}
+                        sx={{ mt: theme.spacing(3) }}
+                    >
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="name"
+                                    label="Name"
+                                    name="name"
+                                    autoComplete="name"
+                                    onChange={handleInputChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    onChange={handleInputChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                    onChange={handleInputChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="confirmPassword"
+                                    label="Confirm Password"
+                                    type="password"
+                                    id="confirmPassword"
+                                    onChange={handleInputChange}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: theme.spacing(3), mb: theme.spacing(2) }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="/login" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 }
-
-export default Register;
