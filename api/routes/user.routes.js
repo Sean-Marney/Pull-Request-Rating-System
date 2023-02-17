c0e1c76d0d7f5f526d45567836543cc953789b10
c1986023 <Sonyw910i->
2023-02-17 16:14:52 +0000

    Created routes for user requests

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,8 +1,20 @@
 const express = require("express");
-const { getUsers } = require("../controllers/user.controller");
-const verifyJWTToken = require("../middleware/verifyJWT");
 const router = express.Router();
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require("../controllers/manageUsers.controller");
+const verifyJWTToken = require("../middleware/verifyJWT");
 
-router.get("/users", verifyJWTToken, getUsers);
+
+// router.get("/users", verifyJWTToken, getUsers);
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.post("/create", createUser);
+router.patch("/update/:id", updateUser);
+router.delete("/delete/:id", deleteUser);
 
 module.exports = router;
