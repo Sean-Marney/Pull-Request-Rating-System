8c2b7412f8e3fafddb7f68b24fb4bbfded7044e8
c1986023 <Sonyw910i->
2023-02-18 12:43:47 +0000

    Modified the createUser method to save user's role as Developer when a Manager creates a user

--- api/controllers/manageUsers.controller.js
+++ api/controllers/manageUsers.controller.js
@@ -1,80 +1,86 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 // Get all users
 const getUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 };
 
 // Get a user by ID
 const getUserById = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res
-        .status(404)
-        .json({ message: "User with that ID was not found" });
-    }
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res
+                .status(404)
+                .json({ message: "User with that ID was not found" });
+        }
 
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 // Create a user
 const createUser = async (req, res) => {
-  try {
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    await user.save();
+    try {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: await bcrypt.hash(req.body.password, 10),
+            hasRole: "Developer",
+        });
+        await user.save();
 
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 // Update a user
 const updateUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).json({ message: "User with that ID not found" });
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res
+                .status(404)
+                .json({ message: "User with that ID not found" });
+        }
+
+        user.name = req.body.name;
+        user.email = req.body.email;
+        user.password = req.body.password;
+        await user.save();
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-
-    user.name = req.body.name;
-    user.email = req.body.email;
-    user.password = req.body.password;
-    await user.save();
-
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 
 // Delete a user
 const deleteUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).json({ message: "User with that ID not found" });
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res
+                .status(404)
+                .json({ message: "User with that ID not found" });
+        }
+
+        await user.remove();
+
+        res.status(200).json({ message: "User deleted" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-
-    await user.remove();
-
-    res.status(200).json({ message: "User deleted" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 
 module.exports = {
@@ -82,5 +88,5 @@ module.exports = {
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
 };
