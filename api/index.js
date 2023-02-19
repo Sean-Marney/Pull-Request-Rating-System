ce343a29a547a19ffaa991250e14b034317d977e
c2063799 <@>
2023-02-19 19:59:13 +0000

    Currently working nodemailer code

--- api/index.js
+++ api/index.js
@@ -1,18 +1,64 @@
 // server.js
 const express = require("express");
 const connectDB = require("./config/db");
-require("dotenv").config();
+const dotenv = require("dotenv").config();
 const cors = require("cors");
 const rewardsRoute = require("./routes/rewards.routes");
 const pullRequestsRoute = require("./routes/pullRequests");
 
 const app = express();
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.json());
+
+
+
+
+
+
+//Nodemailer code
+//Currently only sending email though postman.
+const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+        user: 'tinom101@outlook.com', //dummy email address
+        pass: 'templejax12',
+    },
+});
+
+app.post('/send-email', (req, res) => {
+    const { name, email, message } = req.body;
+
+    const mailOptions = {
+        from: email,
+        to: 'tinmsipa2@gmail.com',
+        subject: 'New Contact Form Submission',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('Email sent');
+        }
+    });
+});
+
+
+
+
 
 // connect database
 connectDB();
 
 app.use(express.json({ extended: false }));
 app.use(cors());
+app.use(express.json())
 
 app.get("/", (req, res) => res.send("Server up and running"));
 app.use("/pullrequests", pullRequestsRoute);
