310e16c5abfa20d9ddd1c3f3da9c00a7f1f2d853
Sean Marney <marneys@cardiff.ac.uk>
2023-02-22 16:30:34 +0000

    Fixed Ose's code by adding a map so that questions are being returned from the database rather than having it hard-coded

--- ui/src/components/pages/FAQPage/FAQ.js
+++ ui/src/components/pages/FAQPage/FAQ.js
@@ -1,99 +1,92 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {
-    Typography,
-    Box,
-    Card,
-    CardContent,
-    CardActions,
+  Typography,
+  Box,
+  Card,
+  CardContent,
+  CardActions,
 } from "@material-ui/core";
 // import {ExpandMore} from "@material-ui/icons";
-import {Collapse, IconButton} from "@mui/material";
-import { styled } from '@mui/material/styles';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
+import { Collapse, IconButton } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function ManageFaqs() {
-    const [question, setQuestion] = useState(null);
+  const [question, setQuestion] = useState(null);
 
-    useEffect(() => {
-        getFaqs();
-    }, []);
+  useEffect(() => {
+    getFaqs();
+  }, []);
 
-    const getFaqs = async () => {
-        // Get faqs
-        const res = await axios.get("http://localhost:8000/management/manageFaqs");
+  const getFaqs = async () => {
+    // Get faqs
+    const res = await axios.get("http://localhost:8000/management/manageFaqs");
 
-        // Set to state
-        setQuestion(res.data);
-    };
+    // Set to state
+    setQuestion(res.data);
+  };
 
+  const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
 
+  const [expanded, setExpanded] = React.useState(false);
 
-    const ExpandMore = styled((props) => {
-        const { expand, ...other } = props;
-        return <IconButton {...other} />;
-    })(({ theme, expand }) => ({
-        transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    }));
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
+  return (
+    <div>
+      <Box padding={3}>
+        <Typography variant="h4">
+          <b>FAQ</b>
+        </Typography>
+      </Box>
 
-    const [expanded, setExpanded] = React.useState(false);
-
-    const handleExpandClick = () => {
-            setExpanded(!expanded);
-    };
-
-        return (
-        <div>
-            <Box padding={3}>
-                <Typography variant="h4">
-                    <b>FAQ</b>
+      <Box padding={5}>
+        {/* Get all FAQs from database and display on a card */}
+        {question &&
+          question.map((q) => (
+            <Card sx={{ maxWidth: 345 }} key={q._id}>
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  Question
                 </Typography>
-                </Box>
-
-            <Box padding={5}>
-                {/* Get all FAQs from database and display on a card */}
-                {question &&(
-                <Card sx={{ maxWidth: 345 }} key={question._id}>
-                    <CardContent>
-                        <Typography variant="body2" color="text.secondary">
-                            Question
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {question.question}
-                            test question
-                        </Typography>
-                    </CardContent>
-                    <CardActions disableSpacing>
-
-                        <ExpandMore
-                            expand={expanded}
-                            onClick={handleExpandClick}
-                            aria-expanded={expanded}
-                            aria-label="show more"
-                        >
-                            answer
-                            <ExpandMoreIcon />
-                        </ExpandMore>
-                    </CardActions>
-                    <Collapse in={expanded} timeout="auto" unmountOnExit>
-                        <CardContent>
-                            <Typography variant="body2" color="text.secondary">ANSWER</Typography>
-                            <Typography paragraph>
-                                {question.answer}
-                                test answer
-                            </Typography>
-
-                        </CardContent>
-                    </Collapse>
-                </Card>
-                )}
-            </Box>
-        </div>
-    );
+                <Typography variant="body2" color="text.secondary">
+                  {q.question}
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+                <ExpandMore
+                  expand={expanded}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                >
+                  answer
+                  <ExpandMoreIcon />
+                </ExpandMore>
+              </CardActions>
+              <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <Typography variant="body2" color="text.secondary">
+                    ANSWER
+                  </Typography>
+                  <Typography paragraph>{q.answer}</Typography>
+                </CardContent>
+              </Collapse>
+            </Card>
+          ))}
+      </Box>
+    </div>
+  );
 }
