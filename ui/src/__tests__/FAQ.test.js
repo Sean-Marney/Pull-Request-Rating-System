b11521629673603760e1959eb28111af38603033
c1989961 <InegbedionOI@cardiff.ac.uk>
2023-02-23 17:26:40 +0000

    fixed faq ui test

--- ui/src/__tests__/FAQ.test.js
+++ ui/src/__tests__/FAQ.test.js
@@ -11,6 +11,7 @@ describe("Testing developer's FAQ page at /FAQ", () => {
     it("should display data on faq card", async () => {
         const mockData = [
             { _id: "1", question: "Question Test", answer: "Answer Test" },
+            { _id: "2", question: "Question Test 2", answer: "Answer Test 2" },
         ];
 
         axios.get.mockResolvedValue({ data: mockData }); // mock the response of axios
@@ -18,9 +19,13 @@ describe("Testing developer's FAQ page at /FAQ", () => {
         render(<FAQ />);
 
         const question = await screen.findByText("Question Test");
+        const question2 = await screen.findByText("Question Test2");
         const answer = await screen.findByText("Answer Test");
+        const answer2 = await screen.findByText("Answer Test2");
 
         expect(question).toBeInTheDocument();
+        expect(question2).toBeInTheDocument();
         expect(answer).toBeInTheDocument();
+        expect(answer2).toBeInTheDocument();
     });
 });
