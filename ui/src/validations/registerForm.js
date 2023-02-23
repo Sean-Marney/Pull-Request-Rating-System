c74e9145b6ce6b42895f7ad02af2a5fd9f1e4951
c1986023 <Sonyw910i->
2023-02-23 20:46:01 +0000

    Added regex expression to validate email and name for registering Manager

--- ui/src/validations/registerForm.js
+++ ui/src/validations/registerForm.js
@@ -4,21 +4,22 @@ YupPassword(yup);
 
 // Used to validate the create user form and update form
 export default yup.object().shape({
-    name: yup.string().max(50).required("Please enter your full name"),
+    name: yup
+        .string()
+        .matches(/^[a-zA-Z ]*$/, "Please enter your full name")
+        .strict()
+        .required("Please enter your full name"),
     email: yup
         .string()
         .email("Please enter a valid email address")
+        .matches(
+            /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+            "Please enter your email"
+        )
         .required("Email is required"),
     password: yup
         .string()
-        .min(
-            8,
-            "password must contain 8 or more characters with at least one of each: uppercase, lowercase, number and special"
-        )
-        .minLowercase(1)
-        .minUppercase(1)
-        .minNumbers(1)
-        .minSymbols(1)
+        .min(8, "Password must be at least 8 characters long")
         .required("Please provide a password"),
     confirmPassword: yup
         .string()
