b6f6f441b5cba48918abe08f8164962b6d101464
c1986023 <Sonyw910i->
2023-02-24 11:18:39 +0000

    Updated validations on login page

--- ui/src/validations/loginForm.js
+++ ui/src/validations/loginForm.js
@@ -14,5 +14,10 @@ export default yup.object().shape({
         .required("Email is required"),
     password: yup
         .string()
-        .required("Please provide correct password"),
+        .min(8, "Password must be at least 8 characters long")
+        .matches(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?])[A-Za-z\d!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]{8,}$/,
+            "Password must contain at least one uppercase letter, one lowercase letter, one number and one special case character"
+        )
+        .required("Please provide a password"),
 });
