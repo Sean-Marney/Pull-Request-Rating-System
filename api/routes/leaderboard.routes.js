ba5ee7d406334f1c2ca3d70aacfaa9d25e9142fe
c2063799 <@>
2023-02-27 10:11:30 +0000

    Added a test for the leaderboard.

--- api/routes/leaderboard.routes.js
+++ api/routes/leaderboard.routes.js
@@ -1,18 +1,7 @@
-// backend/routes/users.js
-const User = require("../models/user.model");
-
 const express = require('express');
 const router = express.Router();
+const leaderboardController = require('../controllers/leaderboard.controller.js');
 
-
-router.get('/leaderboard', async (req, res) => {
-    try {
-        const leaderboard = await User.find().sort({ stars: -1 }).limit(10).select("name stars");
-        res.json(leaderboard);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
-    }
-});
+router.get('/leaderboard', leaderboardController.getLeaderboard);
 
 module.exports = router;
