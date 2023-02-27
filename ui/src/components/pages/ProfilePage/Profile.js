27a514d78e5395be57a5643ceabb949403fdf7d3
c1989961 <InegbedionOI@cardiff.ac.uk>
2023-02-27 02:16:21 +0000

    added components to the profile page

--- ui/src/components/pages/ProfilePage/Profile.js
+++ ui/src/components/pages/ProfilePage/Profile.js
@@ -1,10 +1,54 @@
 import React, { useState, useEffect } from "react";
+import Box from '@mui/material/Box';
+
+// const useStyles = makeStyles((theme) => ({
+//     Box: {
+//         width: 300,
+//         height: 300,
+//         backgroundColor: 'primary.dark'
+//     }
+// }));
 
 export default function ManageProfiles() {
 
     return (
+        <div>
+        <Box padding={3}>
         <h2>
             Manage Profile
         </h2>
+        </Box>
+        <Box padding={3}>
+        <h4>
+            Name
+        </h4>
+        <h4>
+            Status
+        </h4>
+        <h4>
+            company
+        </h4>
+        </Box>
+        <Box padding={3}>
+        <h4>
+            bio
+        </h4>
+        </Box>
+        <Box padding={3}>
+        <h4>
+            stars Aquired 
+        </h4>
+        </Box>
+        <Box padding={3}>
+        <h4>
+            Rewards Gotten
+        </h4>
+        </Box>
+        <Box padding={3}>
+        <h4>
+            Change password
+        </h4>
+        </Box>
+        </div>
     );
 }
