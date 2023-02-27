cb622f523ee8285f32e95fe3907848e4935fb7d3
c1986023 <Sonyw910i->
2023-02-27 15:34:52 +0000

    Renamed the history file in controller and routes to pullRequestHistory and organized the code in the files to avoid redundancy

--- api/controllers/pullRequestHistory.controller.js
+++ api/controllers/pullRequestHistory.controller.js
@@ -3,42 +3,19 @@ const PullRequest = require("../models/pullRequest.model");
 // Reads the pull request for a user
 // Takes in the id as a parameter and then returns the pull requests
 const getPullRequestsForUser = async (req, res) => {
-  try {
-    const  parameter  = req.params;
-    var sortByDate = { date: -1 };
-    const pullRequests = await PullRequest.find(  { user_id: parameter.id} ).sort(sortByDate);
-    res.status(200).json(pullRequests);
-  } catch (error) {
-    console.log(error);
-    res.status(404).json({ message: error.message });
-  }
-};
-
-// Update rating
-const updateRatingForPullRequest = async (req, res) => {
     try {
-        const pullRequest = await PullRequest.updateOne(
-            { _id: req.params.id },
-            {
-                $set: {
-                    ratings: req.body.rating,
-                },
-            }
-        );
-
-        if (!pullRequest) {
-            return res
-                .status(404)
-                .json({ message: "Pull request with that ID not found" });
-        }
-        res.status(200).json(pullRequest);
+        const parameter = req.params;
+        var sortByDate = { date: -1 };
+        const pullRequests = await PullRequest.find({
+            user_id: parameter.id,
+        }).sort(sortByDate);
+        res.status(200).json(pullRequests);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        console.log(error);
+        res.status(404).json({ message: error.message });
     }
 };
 
-
 module.exports = {
     getPullRequestsForUser,
-    updateRatingForPullRequest,
 };
--- api/routes/pullRequestHistory.routes.js
+++ api/routes/pullRequestHistory.routes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const {
     getPullRequestsForUser,
-    updateRatingForPullRequest,
 } = require("../controllers/pullRequestHistory.controller");
 
 const router = express.Router();
 
 // gets the pull request for a user
 router.get("/history/:id", getPullRequestsForUser);
-router.put("/update/:id", updateRatingForPullRequest);
 
 module.exports = router;
