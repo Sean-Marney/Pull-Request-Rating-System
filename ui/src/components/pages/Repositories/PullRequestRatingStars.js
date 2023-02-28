566d1e0dbeafeb96b8bf04dd4799e7102eb61a60
c1986023 <Sonyw910i->
2023-02-28 21:12:37 +0000

    Created a PullRequestRatingStars component that displays a star rating for a pull request.

---
+++ ui/src/components/pages/Repositories/PullRequestRatingStars.js
@@ -0,0 +1,30 @@
+import { Rating } from "@mui/material";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        position: "absolute",
+        top: 5,
+        right: 5,
+    },
+});
+
+const PullRequestRatingStars = ({ rating }) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <Rating
+                name="pull-request-rating"
+                value={rating?.overall ? rating.overall : 0}
+                readOnly
+                sx={{
+                    "& .MuiRating-iconEmpty": {
+                        borderColor: "black",
+                    },
+                }}
+            />
+        </div>
+    );
+};
+
+export default PullRequestRatingStars;
