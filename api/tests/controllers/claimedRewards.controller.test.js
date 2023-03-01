e9267a9351fbfd5d95c95573cf06887c32ba0166
Sean Marney <marneys@cardiff.ac.uk>
2023-03-01 16:00:45 +0000

    Created tests for controller methods

--- api/tests/controllers/claimedRewards.controller.test.js
+++ api/tests/controllers/claimedRewards.controller.test.js
@@ -3,6 +3,8 @@ const sinon = require("sinon");
 const ClaimedRewards = require("../../models/claimedRewards.model");
 const {
   getClaimedRewards,
+  saveClaimedReward,
+  updateArchiveStatus,
 } = require("../../controllers/claimedRewards.controller");
 
 describe("testing getClaimedRewards controller method", function () {
@@ -63,5 +65,126 @@ describe("testing getClaimedRewards controller method", function () {
       message: error.message,
     });
     findStub.restore();
+  });
+});
+
+describe("testing saveClaimedReward controller method", function () {
+  it("should save the claimed reward and return a 201 status code", async function () {
+    // Mocking claimed rewards
+    const req = {
+      body: {
+        rewardId: "1",
+        rewardName: "Reward 1",
+        userId: "user1",
+        userEmail: "user1@example.com",
+        dateClaimed: "2022-01-01",
+      },
+    };
+    const res = {
+      status: sinon.stub().returns({
+        json: sinon.stub().withArgs(sinon.match.has("reward_id", "1")),
+      }),
+    };
+    // Saving mocked rewards
+    const saveStub = sinon.stub(ClaimedRewards.prototype, "save").resolves({
+      reward_id: "1",
+      reward_name: "Reward 1",
+      user_id: "user1",
+      user_email: "user1@example.com",
+      date_claimed: "2022-01-01",
+    });
+
+    await saveClaimedReward(req, res);
+
+    // Assert that the save method has been called and it has a 201 status code
+    assert.ok(saveStub.calledOnce);
+    assert.ok(res.status.calledWith(201));
+    saveStub.restore();
+  });
+
+  it("should return a 500 status with an error message if an error occurs", async function () {
+    const error = new Error("Internal server error");
+    const saveStub = sinon
+      .stub(ClaimedRewards.prototype, "save")
+      .rejects(error);
+    const req = {
+      body: {
+        rewardId: "1",
+        rewardName: "Reward 1",
+        userId: "user1",
+        userEmail: "user1@example.com",
+        dateClaimed: "2022-01-01",
+      },
+    };
+    const res = {
+      status: sinon.stub().returns({
+        json: sinon.stub().withArgs({
+          message: error.message,
+        }),
+      }),
+    };
+
+    await saveClaimedReward(req, res);
+
+    assert.ok(saveStub.calledOnce);
+    assert.ok(res.status.calledWith(500));
+    assert.deepStrictEqual(res.status().json.firstCall.args[0], {
+      message: error.message,
+    });
+    saveStub.restore();
+  });
+});
+
+describe("testing updateArchiveStatus controller method", () => {
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("should return a 404 status with an error message if the claimed reward is not found", async () => {
+    const req = { params: { id: "123" } };
+    const res = { status: sinon.stub(), json: sinon.stub() };
+
+    sandbox.stub(ClaimedRewards, "findById").resolves(null);
+    res.status.returns(res);
+
+    await updateArchiveStatus(req, res);
+
+    sinon.assert.calledOnce(ClaimedRewards.findById);
+    sinon.assert.calledWith(ClaimedRewards.findById, "123");
+    sinon.assert.calledOnce(res.status);
+    sinon.assert.calledWith(res.status, 404);
+    sinon.assert.calledOnce(res.json);
+    sinon.assert.calledWith(
+      res.json,
+      sinon.match({ message: "Reward with that ID not found" })
+    );
+  });
+
+  it("should return a 500 status with an error message if an error occurs", async () => {
+    const req = { params: { id: "123" } };
+    const res = { status: sinon.stub(), json: sinon.stub() };
+
+    sandbox
+      .stub(ClaimedRewards, "findById")
+      .rejects(new Error("Database error"));
+    res.status.returns(res);
+
+    await updateArchiveStatus(req, res);
+
+    sinon.assert.calledOnce(ClaimedRewards.findById);
+    sinon.assert.calledWith(ClaimedRewards.findById, "123");
+    sinon.assert.calledOnce(res.status);
+    sinon.assert.calledWith(res.status, 500);
+    sinon.assert.calledOnce(res.json);
+    sinon.assert.calledWith(
+      res.json,
+      sinon.match({ message: "Database error" })
+    );
   });
 });
