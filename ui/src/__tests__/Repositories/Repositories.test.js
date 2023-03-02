e44286048e1a93c73fa9edf73c8d3d06c0ed7230
c1986023 <Sonyw910i->
2023-03-02 09:47:48 +0000

    Added further tests to the repository component

--- ui/src/__tests__/Repositories/Repositories.test.js
+++ ui/src/__tests__/Repositories/Repositories.test.js
@@ -1,18 +1,60 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import axios from "axios";
 import RepositoryList from "../../components/pages/Repositories/Repositories";
-import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router";
+import "@testing-library/jest-dom/extend-expect";
 
 jest.mock("axios");
 
-describe("RepositoryList", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
+const mockedAxios = axios;
 
-  test("it renders without errors", () => {
-    render(<RepositoryList />);
-    expect(screen.getByText("Pull Requests")).toBeInTheDocument();
-  });
+describe("RepositoryList", () => {
+    beforeEach(() => {
+        axios.get.mockResolvedValue({
+            data: {
+                databasePullRequests: [
+                    {
+                        id: 1,
+                        title: "Test Pull Request",
+                        repo: "test-repo",
+                        ratings: {},
+                        createdAt: "2022-03-01T00:00:00.000Z",
+                    },
+                ],
+                repos: [{ id: 1, name: "test-repo" }],
+            },
+        });
+    });
+
+    it('renders the "All Pull Requests" option and a list of pull requests', async () => {
+        render(
+            <MemoryRouter>
+                <RepositoryList />
+            </MemoryRouter>
+        );
+
+        const allOption = screen.getByText("All Pull Requests");
+        expect(allOption).toBeInTheDocument();
+
+        // const pullRequestTitle = await screen.findByText("Test Pull Request");
+        // expect(pullRequestTitle).toBeInTheDocument();
+    });
+});
+
+test("renders RepositoryList without crashing", () => {
+    render(
+        <MemoryRouter>
+            <RepositoryList />
+        </MemoryRouter>
+    );
+});
+
+test("renders 'All Pull Requests' by default", () => {
+    const { getByText } = render(
+        <MemoryRouter>
+            <RepositoryList />
+        </MemoryRouter>
+    );
+    expect(getByText("All Pull Requests")).toBeInTheDocument();
 });
