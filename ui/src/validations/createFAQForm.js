089ca714325b1096b3062e6cf9f78c3056884eed
c1989961 <InegbedionOI@cardiff.ac.uk>
2023-03-03 14:57:28 +0000

    added spacing to validation

--- ui/src/validations/createFAQForm.js
+++ ui/src/validations/createFAQForm.js
@@ -4,7 +4,9 @@ import * as yup from "yup";
 export default yup.object().shape({
     question: yup
     .string()
-    .matches(/^[a-zA-Z0-9.?]*$/, "Please enter an answer")
+    // .matches(/^[a-zA-Z0-9 .?]*$/, "Please enter an answer")
+    .matches(/^[a-zA-Z0-9.?]+[a-zA-Z0-9.? ]*[a-zA-Z0-9.?]*$/, "Please enter an answer")
+    // /^[a-zA-Z0-9]+[a-zA-Z0-9 ]*[a-zA-Z0-9]+$/gm
     .required("You must enter a question"),
     answer: yup
     .string()
