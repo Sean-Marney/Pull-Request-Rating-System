c00a7f03bd1efe63d2f1184155e5e3c15c13f6d9
Sean Marney <marneys@cardiff.ac.uk>
2023-03-06 21:10:22 +0000

    Stars rewarded from a pull request rating is now added to the stars field in the user object of the user that is being reviewed

--- api/controllers/rating.controller.js
+++ api/controllers/rating.controller.js
@@ -1,57 +1,81 @@
 const PullRequest = require("../models/pullRequest.model");
 const math = require("mathjs");
+const axios = require("axios");
 
 const createRating = async (req, res) => {
-    try {
-        // Validate input
-        if (!req.body.rating || !req.body.rating_complete) {
-            return res
-                .status(400)
-                .json({ message: "Missing rating input fields" });
-        }
-
-        // Calculate overall rating
-        const ratings = Object.values(req.body.rating);
-        const numRatings = ratings.length;
-        if (numRatings === 0) {
-            return res.status(400).json({ message: "No ratings provided" });
-        }
-        const ratingAverage = math.ceil(math.sum(ratings) / numRatings);
-        const ratingSum = math.sum(ratings);
-
-        // Update pull request
-        const result = await PullRequest.updateOne(
-            { _id: req.params.id },
-            {
-                $set: {
-                    ratings: {
-                        ...req.body.rating,
-                        overall: ratingSum,
-                        average: ratingAverage,
-                    },
-                    rating_complete: req.body.rating_complete,
-                },
-            }
-        );
-
-        // Check if pull request was found
-        if (result.nModified === 0) {
-            return res
-                .status(404)
-                .json({ message: "Pull request with that ID not found" });
-        }
-
-        // Return success response
-        res.status(200).json({ message: "Rating updated successfully" });
-    } catch (error) {
-        // Handle errors
-        console.error(error);
-        res.status(500).json({
-            message: "An error occurred while updating rating",
-        });
+  try {
+    // Validate input
+    if (!req.body.rating || !req.body.rating_complete) {
+      return res.status(400).json({ message: "Missing rating input fields" });
     }
+
+    // Calculate overall rating
+    const ratings = Object.values(req.body.rating);
+    const numRatings = ratings.length;
+    if (numRatings === 0) {
+      return res.status(400).json({ message: "No ratings provided" });
+    }
+    const ratingAverage = math.ceil(math.sum(ratings) / numRatings);
+    const ratingSum = math.sum(ratings);
+
+    // Update pull request
+    const result = await PullRequest.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          ratings: {
+            ...req.body.rating,
+            overall: ratingSum,
+            average: ratingAverage,
+          },
+          rating_complete: req.body.rating_complete,
+        },
+      }
+    );
+
+    // Get user who's pull request is being rated
+    const pullRequest = await PullRequest.find({ _id: req.params.id });
+    try {
+      const getUserById = await axios.get(
+        `http://localhost:8000/management/users/${pullRequest[0].user_id}`
+      );
+      const user = getUserById.data;
+
+      const updatedStarCount = user.stars + ratingSum;
+
+      // Update user object with added stars
+      await axios.patch(
+        `http://localhost:8000/management/users/update/${user._id}`,
+        {
+          name: user.name,
+          email: user.email,
+          password: user.password,
+          hasRole: user.hasRole,
+          stars: updatedStarCount,
+        }
+      );
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+
+    // Check if pull request was found
+    if (result.nModified === 0) {
+      return res
+        .status(404)
+        .json({ message: "Pull request with that ID not found" });
+    }
+
+    // Return success response
+    res.status(200).json({ message: "Rating updated successfully" });
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).json({
+      message: "An error occurred while updating rating",
+    });
+  }
 };
 
 module.exports = {
-    createRating,
+  createRating,
 };
