464f45fa43f6ed82ffa5907c19b759652e30b58c
c1986023 <Sonyw910i->
2023-03-07 19:32:36 +0000

    Refactored the error messages

--- ui/src/validations/createTrackerForm.js
+++ ui/src/validations/createTrackerForm.js
@@ -1,10 +1,11 @@
 import * as yup from "yup";
 
-// Used to validate the create tracker form and update form
+// Used to validate the create tracker form and update tracker form
 export default yup.object().shape({
     name: yup
         .string()
-        .matches(/^[a-zA-Z ]*$/, "Please enter the tracker name")
+        .matches(/^[a-zA-Z ]*$/, "Tracker name may only contain letters")
         .strict()
+        .max(20, "Tracker name must be max 50 characters long")
         .required("Please enter the tracker name"),
 });
