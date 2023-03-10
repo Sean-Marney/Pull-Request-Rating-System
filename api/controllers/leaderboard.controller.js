33f96b21381ba9d5f218f6cc5dc5b2033781a86c
c2063799 <@>
2023-03-10 10:18:04 +0000

    showing developers only after merge

--- api/controllers/leaderboard.controller.js
+++ api/controllers/leaderboard.controller.js
@@ -1,7 +1,8 @@
+
 const User = require("../models/user.model");
 exports.getLeaderboard = async (req, res) => {
   try {
-    const leaderboard = await User.find()
+    const leaderboard = await User.find({ hasRole: "Developer" })
       .sort({ totalStarsEarned: -1 })
       .limit(10)
       .select("name totalStarsEarned");
