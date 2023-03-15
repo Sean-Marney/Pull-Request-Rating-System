723aa352a848108b2346a3aea540dd4c0dac176b
c1986023 <Sonyw910i->
2023-03-15 22:46:39 +0000

    Added the functionality that the user is notified if the email they entered is not registered to an account and password entered is incorrect

--- ui/src/components/pages/signIn/Login.js
+++ ui/src/components/pages/signIn/Login.js
@@ -23,128 +23,141 @@ import { useState } from "react";
 const theme = createTheme();
 
 export default function SignIn() {
-  const navigate = useNavigate();
-  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
-  const { request } = useAxiosInstance();
-  const [error, setError] = useState({});
-  const [user, setUser] = React.useState({
-    email: "",
-    password: "",
-  });
+    const navigate = useNavigate();
+    const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+    const { request } = useAxiosInstance();
+    const [error, setError] = useState({});
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+    });
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUser((prevUser) => ({
-      ...prevUser,
-      [name]: value,
-    }));
-  };
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUser((prevUser) => ({
+            ...prevUser,
+            [name]: value,
+        }));
+    };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-    try {
-      await validateLoginForm.validate(user, { abortEarly: false });
+        try {
+            // await validateLoginForm.validate(user, { abortEarly: false });
 
-      const response = await request({
-        method: "post",
-        url: "/login",
-        data: { ...user },
-      });
-      if (response.data.token) {
-        setCookie("token", response.data.token, { path: "/" });
-        setCookie("role", response.data.hasRole, { path: "/" });
-        setCookie("user", user.email, { path: "/" });
-        navigate("/");
-      }
-    } catch (error) {
-      const errors = {};
-      if (error instanceof yup.ValidationError) {
-        error.inner.forEach((e) => {
-          errors[e.path] = e.message;
-        });
-        setError(errors);
-      }
-    }
-  };
+            const response = await request({
+                method: "post",
+                url: "/login",
+                data: { ...user },
+            });
+            if (response.data.token) {
+                setCookie("token", response.data.token, { path: "/" });
+                setCookie("role", response.data.hasRole, { path: "/" });
+                setCookie("user", user.email, { path: "/" });
+                navigate("/");
+            }
+        } catch (error) {
+            const errors = {};
+            if (error.response && error.response.data.message) {
+                const errorMessage = error.response.data.message.toLowerCase();
+                if (errorMessage.includes("email")) {
+                    errors.email =
+                        "The email address you entered isn't connected to an account";
+                } else {
+                    errors.password =
+                        "The password that you've entered is incorrect";
+                }
+            } else if (error instanceof yup.ValidationError) {
+                error.inner.forEach((e) => {
+                    errors[e.path] = e.message;
+                });
+            }
+            setError(errors);
+        }
+    };
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <InputLabel htmlFor="email">Email</InputLabel>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              name="email"
-              onChange={handleInputChange}
-              autoComplete="email"
-              autoFocus
-            />
-            {error.email && <div style={{ color: "red" }}>{error.email}</div>}
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              type="password"
-              id="password"
-              onChange={handleInputChange}
-              autoComplete="current-password"
-            />
-            {error.password && (
-              <div style={{ color: "red" }}>{error.password}</div>
-            )}
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="/dashboard" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
+                        noValidate
+                        sx={{ mt: 1 }}
+                    >
+                        <InputLabel htmlFor="email">Email</InputLabel>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            name="email"
+                            onChange={handleInputChange}
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        {error.email && (
+                            <div style={{ color: "red" }}>{error.email}</div>
+                        )}
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            type="password"
+                            id="password"
+                            onChange={handleInputChange}
+                            autoComplete="current-password"
+                        />
+                        {error.password && (
+                            <div style={{ color: "red" }}>{error.password}</div>
+                        )}
+                        <FormControlLabel
+                            control={
+                                <Checkbox value="remember" color="primary" />
+                            }
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="/dashboard" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="/register" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
 }
