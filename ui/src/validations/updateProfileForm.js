f434b4112ef95b06826753c7e90b24bb806bf525
c1989961 <InegbedionOI@cardiff.ac.uk>
2023-03-17 09:52:07 +0000

    added validation to update profile form

---
+++ ui/src/validations/updateProfileForm.js
@@ -0,0 +1,24 @@
+import * as yup from "yup";
+
+// Used to validate the  update form
+export default yup.object().shape({
+    name: yup
+        .string()
+        .matches(/^[a-zA-Z 0-9]*$/, "Please enter the right format")
+        .strict()
+        .required("Please enter your full name"),
+    // email: yup
+    //     .string()
+    //     .email("Please enter a valid email address")
+    //     .matches(
+    //         /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+    //         "Email form is wrong"
+    //     )
+    //     .required("Email is required"),
+    bio: yup
+        .string()
+        .matches(
+          /^[a-zA-Z0-9 .!()]*$/,
+          "Bio cannot contain special characters except for . ! ()"
+        ),
+});
