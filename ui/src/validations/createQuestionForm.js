242ed370fef47d52245b7eb908248ed6d6f80c58
c1989961 <InegbedionOI@cardiff.ac.uk>
2023-03-19 10:55:27 +0000

    created validation for the question form

--- ui/src/validations/createQuestionForm.js
+++ ui/src/validations/createQuestionForm.js
@@ -1,13 +1,9 @@
 import * as yup from "yup";
 
-// Used to validate the create form and update form
+// Used to validate the create form 
 export default yup.object().shape({
     question: yup
     .string()
     .matches(/^[a-zA-Z0-9 ]+[a-zA-Z0-9 ]*[a-zA-Z0-9 ]*[.?]*[a-zA-Z0-9 ]+[a-zA-Z0-9 ]*[a-zA-Z0-9 ]*[.?]*$/, "Question should only have . or ?")
     .required("You must enter a question"),
-    answer: yup
-    .string()
-    .matches(/^[a-zA-Z0-9 ]+[a-zA-Z0-9 ]*[a-zA-Z0-9 ]*[.?]*[a-zA-Z0-9 ]+[a-zA-Z0-9 ]*[a-zA-Z0-9 ]*[.?]*$/, "Answer should only have .")
-    .required("You must enter an answer"),
 });
