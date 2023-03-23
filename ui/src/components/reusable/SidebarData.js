ac64872273a30eb9ce4c7f7ee97e7c1b6bfef6f0
Owain Lansdowne <lansdowneop@cardiff.ac.uk>
2023-03-23 09:50:05 +0000

    Test

--- ui/src/components/reusable/SidebarData.js
+++ ui/src/components/reusable/SidebarData.js
@@ -44,6 +44,11 @@ const Sidebar = ({ children, removeCookie }) => {
             path: "/profile",
             name: "Profile",
             icon: <AccountCircleIcon />,
+        },
+        {
+            path: "/management/users",
+            name: "Profile",
+            icon: <AccountCircleIcon />,
         }
     ];
     return (
