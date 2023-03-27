edbd3b4995c1635d852c86e2f31751251b133f98
c1986023 <Sonyw910i->
2023-03-27 12:07:49 +0100

    Added API routes for sendOTP and verifyOTP in auth routes

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/auth.controller");
+const {
+    registerUser,
+    loginUser,
+    sendOTP,
+    verifyOTP,
+} = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/sendOTP/:email", sendOTP);
+router.post("/forgotpassword/verify-otp/:email/:otp", verifyOTP);
 
 module.exports = router;
