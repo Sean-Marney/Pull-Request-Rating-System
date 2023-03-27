965839642377495766a54a01af6021a12ac02c09
Sean Marney <marneys@cardiff.ac.uk>
2023-03-27 15:20:10 +0100

    Created controller method to get number of pending pull requests

--- api/controllers/managerDashboard.controller.js
+++ api/controllers/managerDashboard.controller.js
@@ -1,5 +1,15 @@
 const PullRequest = require("../models/pullRequest.model");
 
-const getNumberOfPendingPullRequests = async (req, res) => {};
+// Gets the number of pull requests that are pending (i.e. not been rated yet)
+const getNumberOfPendingPullRequests = async (req, res) => {
+  try {
+    const pendingPullRequestCount = await PullRequest.countDocuments({
+      rating_complete: false,
+    });
+    res.status(200).json({ pendingPullRequestCount });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = { getNumberOfPendingPullRequests };
