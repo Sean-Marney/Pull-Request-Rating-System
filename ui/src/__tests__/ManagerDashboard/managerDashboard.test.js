ecc2c46ece30299dae968f19792c4c7b2f305a1c
Sean Marney <marneys@cardiff.ac.uk>
2023-03-30 13:53:17 +0100

    Created UI tests for the manager dashboard component

---
+++ ui/src/__tests__/ManagerDashboard/managerDashboard.test.js
@@ -0,0 +1,79 @@
+import React from "react";
+import axios from "axios";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Dashboard from "../../components/pages/ManagerDashboard/ManagerDashboard";
+
+jest.mock("axios");
+
+describe("Testing manager dashboard component", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render the component with the expected data", () => {
+    render(<Dashboard />);
+
+    expect(screen.getByText("Dashboard")).toBeInTheDocument();
+    expect(screen.getByText("Pending Pull Requests")).toBeInTheDocument();
+    expect(screen.getByText("Claimed Rewards")).toBeInTheDocument();
+    expect(screen.getByText("Top Developer")).toBeInTheDocument();
+  });
+
+  it("should fetch and display the number of pending pull requests", async () => {
+    const mockNumberOfPendingPullRequests = 5;
+    axios.get.mockResolvedValueOnce({ data: mockNumberOfPendingPullRequests });
+    render(<Dashboard />);
+
+    expect(axios.get).toHaveBeenCalledWith(
+      "http://localhost:8000/management/dashboard/get-number-of-pending-pull-requests"
+    );
+
+    const pendingPullRequestsElement = await screen.findByText(
+      `${mockNumberOfPendingPullRequests}`
+    );
+    expect(pendingPullRequestsElement).toBeInTheDocument();
+  });
+
+  it("should fetch and display the number of claimed rewards", async () => {
+    const mockNumberOfClaimedRewards = 10;
+    axios.get.mockResolvedValueOnce({ data: mockNumberOfClaimedRewards });
+    render(<Dashboard />);
+
+    expect(axios.get).toHaveBeenCalledWith(
+      "http://localhost:8000/management/dashboard/get-number-of-claimed-rewards"
+    );
+
+    const claimedRewardsElement = await screen.findByText(
+      `${mockNumberOfClaimedRewards}`
+    );
+    expect(claimedRewardsElement).toBeInTheDocument();
+  });
+
+  it("should fetch and display the list of claimed rewards", async () => {
+    const mockClaimedRewards = [
+      { id: 1, name: "Reward 1" },
+      { id: 2, name: "Reward 2" },
+    ];
+    axios.get.mockResolvedValueOnce({ data: mockClaimedRewards });
+    render(<Dashboard />);
+
+    expect(axios.get).toHaveBeenCalledWith(
+      "http://localhost:8000/management/dashboard/get-claimed-rewards"
+    );
+  });
+
+  it("should fetch and display the top 3 developers", async () => {
+    const mockTopDevelopers = [
+      { id: 1, name: "Developer 1", total_stars: 10 },
+      { id: 2, name: "Developer 2", total_stars: 8 },
+      { id: 3, name: "Developer 3", total_stars: 6 },
+    ];
+    axios.get.mockResolvedValueOnce({ data: mockTopDevelopers });
+    render(<Dashboard />);
+
+    expect(axios.get).toHaveBeenCalledWith(
+      "http://localhost:8000/management/dashboard/get-top-developers"
+    );
+  });
+});
