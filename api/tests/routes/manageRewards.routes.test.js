d17619d3c8f527ad2304a2951f7f6903de85ea88
Sean Marney <marneys@cardiff.ac.uk>
2023-04-16 11:49:07 +0100

    Rewritten deleteRewards test

--- api/tests/routes/manageRewards.routes.test.js
+++ api/tests/routes/manageRewards.routes.test.js
@@ -1,6 +1,9 @@
 const request = require("supertest");
 const app = require("../../index");
 const assert = require("assert");
+const sinon = require("sinon");
+const chai = require("chai");
+const Reward = require("../../models/reward.model");
 
 describe("GET /management/rewards", () => {
   it("should return all rewards and status code 200", async () => {
@@ -41,23 +44,6 @@ describe("GET /management/rewards", () => {
 //       existingReward.starsRequired
 //     );
 //   });
-
-//   it("should return 404 if the reward with the specified ID does not exist", async () => {
-//     // Mocking an invalid reward ID
-//     const invalidId = "123456789012";
-
-//     // Sending a GET request to retrieve the reward with the invalid ID
-//     const getResponse = await request(app).get(
-//       `/management/rewards/${invalidId}`
-//     );
-
-//     // Expect that the status code and response body are as expected
-//     assert.strictEqual(getResponse.statusCode, 404);
-//     assert.deepStrictEqual(
-//       getResponse.body.message,
-//       "Reward with that ID was not found"
-//     );
-//   });
 // });
 
 describe("POST /management/rewards/create", () => {
@@ -81,25 +67,6 @@ describe("POST /management/rewards/create", () => {
       newReward.starsRequired
     );
   });
-
-  // it("should return 500 if there is an error creating the reward", async () => {
-  //   // Mocking an invalid reward object (missing required field)
-  //   const invalidReward = {
-  //     starsRequired: 30,
-  //   };
-
-  //   // Sending a POST request to create the invalid reward
-  //   const response = await request(app)
-  //     .post("/management/rewards/create")
-  //     .send(invalidReward);
-
-  //   // Expect that the status code and response body are as expected
-  //   assert.strictEqual(response.statusCode, 500);
-  //   assert.deepStrictEqual(
-  //     response.body.message,
-  //     "Reward validation failed: rewardName: Path `rewardName` is required."
-  //   );
-  // });
 });
 
 describe("PATCH /management/rewards/update/:id", () => {
@@ -137,29 +104,29 @@ describe("PATCH /management/rewards/update/:id", () => {
       updatedReward.starsRequired
     );
   });
+});
 
-  // it("should return 404 if the reward with the specified ID does not exist", async () => {
-  //   // Mocking an invalid reward ID
-  //   const invalidId = "123456789012";
+describe("DELETE /management/rewards/:id", () => {
+  it("should delete a reward and return status code 200", async () => {
+    // Mocking a reward object
+    const newReward = {
+      rewardName: "New Reward",
+      starsRequired: 30,
+    };
 
-  //   // Mocking updated reward information
-  //   const updatedReward = {
-  //     rewardName: "Updated Reward",
-  //     starsRequired: 30,
-  //   };
+    // Sending a POST request to create the reward
+    const response = await request(app)
+      .post("/management/rewards/create")
+      .send(newReward);
 
-  //   // Sending a PATCH request to update the reward with the invalid ID
-  //   const updateResponse = await request(app)
-  //     .patch(`/management/rewards/update/${invalidId}`)
-  //     .send(updatedReward);
+    // Sending a DELETE request to delete the reward
+    const deleteResponse = await request(app).delete(
+      `/management/rewards/delete/${response.body._id}`
+    );
 
-  //   // Expect that the status code and response body are as expected
-  //   assert.strictEqual(updateResponse.statusCode, 404);
-  //   assert.deepStrictEqual(
-  //     updateResponse.body.message,
-  //     "Reward with that ID not found"
-  //   );
-  // });
+    assert.strictEqual(deleteResponse.statusCode, 200);
+    assert.deepStrictEqual(deleteResponse.body.message, "Reward deleted");
+  });
 });
 
 // describe("DELETE /management/rewards/delete/:id", () => {
@@ -191,22 +158,5 @@ describe("PATCH /management/rewards/update/:id", () => {
 
 //     // Expect that the status code is 404, indicating that the reward does not exist
 //     assert.strictEqual(getResponse.statusCode, 404);
-//   });
-
-//   it("should return 404 if the reward with the specified ID does not exist", async () => {
-//     // Mocking an invalid reward ID
-//     const invalidId = "123456789012";
-
-//     // Sending a DELETE request to delete the reward with the invalid ID
-//     const deleteResponse = await request(app).delete(
-//       `/management/rewards/delete/${invalidId}`
-//     );
-
-//     // Expect that the status code and response body are as expected
-//     assert.strictEqual(deleteResponse.statusCode, 404);
-//     assert.deepStrictEqual(
-//       deleteResponse.body.message,
-//       "Reward with that ID not found"
-//     );
 //   });
 // });
