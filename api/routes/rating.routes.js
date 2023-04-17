ccbc109e96394287be1ca209f02f3d6d09819b3b
c1989961 <InegbedionOI@cardiff.ac.uk>
2023-04-17 01:42:56 +0100

    added route protection to repostiory & rating

--- api/routes/rating.routes.js
+++ api/routes/rating.routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { createRating } = require("../controllers/rating.controller");
 const router = express.Router();
 
-router.put("/update/:id", createRating);
+const {verifyJWTToken, verifyManger, verifyTokenAndAuth} = require('../middleware/verifyJWT')
+router.use(verifyJWTToken)
+
+router.put("/update/:id", verifyManger, createRating);
 
 module.exports = router;
