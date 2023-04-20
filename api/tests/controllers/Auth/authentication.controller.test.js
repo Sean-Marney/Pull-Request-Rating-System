41afbd21c22ad13163c527393f3077985daf6881
c1986023 <Sonyw910i->
2023-04-20 14:16:30 +0100

    Modified the loginUser test to stub res.status and res.cookie and passed that in assertion

--- api/tests/controllers/Auth/authentication.controller.test.js
+++ api/tests/controllers/Auth/authentication.controller.test.js
@@ -130,113 +130,125 @@ describe("registerUser controller method", () => {
 
 // Test suite for the loginUser controller method
 describe("loginUser controller method", () => {
-  // Declare variables to hold stubs
-  let findOneStub, compareStub, signStub;
+    // Declare variables to hold stubs
+    let findOneStub, compareStub, signStub;
 
-  beforeEach(() => {
-    // Create stubs before each test
-    findOneStub = sinon.stub(User, "findOne");
-    compareStub = sinon.stub(bcrypt, "compare");
-    signStub = sinon.stub(jwt, "sign");
-  });
-
-  afterEach(() => {
-    // Restore stubs after each test
-    findOneStub.restore();
-    compareStub.restore();
-    signStub.restore();
-  });
-
-  // Test case for non-existent user
-  it("should return a 401 status and error message for non-existent user", async () => {
-    const req = {
-      body: {
-        email: "non.existent@example.com",
-        password: "password123",
-      },
-    };
-    const res = {
-      status: sinon.stub().returns({ json: sinon.stub() }),
-    };
-
-    // Mock the findOne method of the User model to return null, indicating that there is no user with the email provided in the request
-    findOneStub.resolves(null);
-
-    // Call the controller method with the mocked request and response objects
-    await loginUser(req, res);
-
-    // Assert that the status and json methods of the response object are being called correctly
-    expect(res.status.calledWith(401)).to.be.true;
-    expect(
-      res.status().json.calledWith({
-        message: "User not found with the entered email",
-      })
-    ).to.be.true;
-  });
-
-  // Test case for incorrect password
-  it("should return a 401 status and error message for incorrect password", async () => {
-    const req = {
-      body: {
-        email: "john.doe@example.com",
-        password: "wrongPassword",
-      },
-    };
-    const res = {
-      status: sinon.stub().returns({ json: sinon.stub() }),
-    };
-
-    // Mock the findOne method of the User model to return an object with the email and password that matches the one in the request
-    findOneStub.resolves({
-      email: "john.doe@example.com",
-      password: "hashedPassword123",
+    beforeEach(() => {
+        // Create stubs before each test
+        findOneStub = sinon.stub(User, "findOne");
+        compareStub = sinon.stub(bcrypt, "compare");
+        signStub = sinon.stub(jwt, "sign");
     });
-    // Mock the compare method of bcrypt to return false, indicating that the password in the request is incorrect
-    compareStub.resolves(false);
 
-    // Call the controller method with the mocked request and response objects
-    await loginUser(req, res);
+    afterEach(() => {
+        // Restore stubs after each test
+        findOneStub.restore();
+        compareStub.restore();
+        signStub.restore();
+    });
 
-    // Assert that the status and json methods of the response object are being called correctly
-    expect(res.status.calledWith(401)).to.be.true;
-    expect(res.status().json.calledWith({ message: "Incorrect password" })).to
-      .be.true;
-  });
+    // Test case for successful login
+    it("should successfully log in a user", async () => {
+        const req = {
+            body: {
+                email: "john.doe@example.com",
+                password: "password123",
+            },
+        };
+        const res = {
+            status: sinon.stub().returnsThis(),
+            json: sinon.stub(),
+            cookie: sinon.stub(),
+        };
 
-  // Test case for successful login
-  // it("should successfully log in a user", async () => {
-  //   const req = {
-  //     body: {
-  //       email: "john.doe@example.com",
-  //       password: "password123",
-  //     },
-  //   };
-  //   const res = {
-  //     json: sinon.stub(),
-  //   };
+        // Mock the findOne method of the User model to return an object with the email and password that matches the one in the request
+        findOneStub.resolves({
+            _id: "1",
+            email: "john.doe@example.com",
+            password: "hashedPassword123",
+            hasRole: "Manager",
+        });
+        // Mock the compare method of bcrypt to return true, indicating that the password in the request is correct
+        compareStub.resolves(true);
+        // Mock the sign method of jwt to return a fake token
+        signStub.returns("fakeToken");
 
-  //   // Mock the findOne method of the User model to return an object with the email and password that matches the one in the request
-  //   findOneStub.resolves({
-  //     _id: "1",
-  //     email: "john.doe@example.com",
-  //     password: "hashedPassword123",
-  //     hasRole: "Manager",
-  //   });
-  //   // Mock the compare method of bcrypt to return true, indicating that the password in the request is correct
-  //   compareStub.resolves(true);
-  //   // Mock the sign method of jwt to return a fake token
-  //   signStub.returns("fakeToken");
+        // Call the controller method with the mocked request and response objects
+        await loginUser(req, res);
 
-  //   // Call the controller method with the mocked request and response objects
-  //   await loginUser(req, res);
+        // Assert that the cookie method of the response object is being called correctly
+        expect(
+            res.cookie.calledWith("jwt", "fakeToken", {
+                httpOnly: true,
+                secure: true,
+                sameSite: "None",
+                maxAge: 1000 * 60 * 60 * 24 * 7,
+            })
+        ).to.be.true;
 
-  //   // Assert that the json method of the response object is being called correctly
-  //   expect(
-  //     res.json.calledWith({
-  //       message: "Success",
-  //       token: "Bearer fakeToken",
-  //       hasRole: "Manager",
-  //     })
-  //   ).to.be.true;
-  // });
+        // Assert that the json method of the response object is being called correctly
+        expect(
+            res.json.calledWith({
+                message: "Success",
+                token: "Bearer fakeToken",
+                hasRole: "Manager",
+            })
+        ).to.be.true;
+    });
+
+    // Test case for non-existent user
+    it("should return a 401 status and error message for non-existent user", async () => {
+        const req = {
+            body: {
+                email: "non.existent@example.com",
+                password: "password123",
+            },
+        };
+        const res = {
+            status: sinon.stub().returns({ json: sinon.stub() }),
+        };
+
+        // Mock the findOne method of the User model to return null, indicating that there is no user with the email provided in the request
+        findOneStub.resolves(null);
+
+        // Call the controller method with the mocked request and response objects
+        await loginUser(req, res);
+
+        // Assert that the status and json methods of the response object are being called correctly
+        expect(res.status.calledWith(401)).to.be.true;
+        expect(
+            res.status().json.calledWith({
+                message: "User not found with the entered email",
+            })
+        ).to.be.true;
+    });
+
+    // Test case for incorrect password
+    it("should return a 401 status and error message for incorrect password", async () => {
+        const req = {
+            body: {
+                email: "john.doe@example.com",
+                password: "wrongPassword",
+            },
+        };
+        const res = {
+            status: sinon.stub().returns({ json: sinon.stub() }),
+        };
+
+        // Mock the findOne method of the User model to return an object with the email and password that matches the one in the request
+        findOneStub.resolves({
+            email: "john.doe@example.com",
+            password: "hashedPassword123",
+        });
+        // Mock the compare method of bcrypt to return false, indicating that the password in the request is incorrect
+        compareStub.resolves(false);
+
+        // Call the controller method with the mocked request and response objects
+        await loginUser(req, res);
+
+        // Assert that the status and json methods of the response object are being called correctly
+        expect(res.status.calledWith(401)).to.be.true;
+        expect(res.status().json.calledWith({ message: "Incorrect password" }))
+            .to.be.true;
+    });
 });
